6621e4ec8d855f2bcf1564f8c2bab10afde5fc87
Daniel Dedek <74773330+danieldedek@users.noreply.github.com>
2026-03-18 10:05:52 +0100

    Update canary.py

--- app/canary.py
+++ app/canary.py
@@ -5,5 +5,10 @@ class Canary:
         self.model_name = "nvidia/canary-180m-flash"
         self.model = None
 
+    def download(self):
+        self.model = nemo_asr.models.ASRModel.from_pretrained(self.model_name)
+
     def transcribe(self, audio_path: str) -> str:
+        if self.model is None:
+            self.download()
         return self.model.transcribe([audio_path])[0]
