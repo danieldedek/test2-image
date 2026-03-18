8dfb83714c362b26bdff9888edb940cb67a5ecfa
Daniel Dedek <74773330+danieldedek@users.noreply.github.com>
2026-03-18 10:05:38 +0100

    Update baseASR.py

--- app/baseASR.py
+++ app/baseASR.py
@@ -2,5 +2,9 @@ from abc import ABC, abstractmethod
 
 class BaseASR(ABC):
     @abstractmethod
+    def download(self):
+        pass
+
+    @abstractmethod
     def transcribe(self, audio_path: str) -> str:
         pass
