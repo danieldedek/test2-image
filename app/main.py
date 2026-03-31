96976965545cdc7b9afd1f7de9a8a73890a3eed4
Daniel Dedek <74773330+danieldedek@users.noreply.github.com>
2026-03-31 22:53:49 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,7 +7,9 @@ import subprocess
 import uuid
 
 app = Flask(__name__)
-UPLOAD_FOLDER = "uploads"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
+
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
