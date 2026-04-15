106ed6ba007a22e0ecc194fe162b35df9a4c7d9a
Daniel Dedek <74773330+danieldedek@users.noreply.github.com>
2026-04-15 10:26:26 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -66,7 +66,7 @@ def index():
     )
 
 
-@app.route("/uploads/<path:filename>")
+@app.route("/uploads/<filename>")
 def uploaded_file(filename):
     return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 
