7d3e5ed60f26581db153c2a73cf1ffa80bdd7989
Daniel Dedek <74773330+danieldedek@users.noreply.github.com>
2026-04-26 10:24:08 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -145,5 +145,5 @@ def delete_file(filename):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True, threaded=True)
     
