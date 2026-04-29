84b69ddc744881665616df0eed2210e056061f43
Daniel Dedek <74773330+danieldedek@users.noreply.github.com>
2026-04-29 21:34:47 +0200

    Update parakeet.py

--- app/parakeet.py
+++ app/parakeet.py
@@ -3,16 +3,12 @@ import torch
 
 
 class Parakeet:
-    def __init__(self, device="cpu"):
+    def __init__(self):
         self.model_name = "nvidia/parakeet-tdt-0.6b-v3"
         self.model = None
-        self.device = device
 
     def download(self):
         self.model = nemo_asr.models.ASRModel.from_pretrained(self.model_name)
-
-        device = "cuda" if self.device == "cuda" and torch.cuda.is_available() else "cpu"
-        self.model = self.model.to(device)
 
     def transcribe(self, audio_path: str):
         if self.model is None:
