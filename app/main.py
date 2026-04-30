768fb1c004f8145e06f8674d9aff8a4d1e44c5db
Daniel Dedek <74773330+danieldedek@users.noreply.github.com>
2026-04-30 15:05:16 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -117,7 +117,15 @@ def index():
                     )
 
                 elif engine == "parakeet":
-                    asr = create_asr_engine(engine)
+                    asr = create_asr_engine(
+                        engine,
+                        device=device,
+                        strategy=strategy,
+                        beam_size=beam_size,
+                        alpha=alpha,
+                        beta=beta,
+                        use_fp16=use_fp16
+                    )
 
                 elif engine == "whisper":
                     asr = create_asr_engine(
