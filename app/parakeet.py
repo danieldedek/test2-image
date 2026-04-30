4a4468c3327b415cd2e270b8f3d329762f370563
Daniel Dedek <74773330+danieldedek@users.noreply.github.com>
2026-04-30 14:36:10 +0200

    Update parakeet.py

--- app/parakeet.py
+++ app/parakeet.py
@@ -2,23 +2,16 @@ import nemo.collections.asr as nemo_asr
 
 
 class Parakeet:
-    def __init__(self, device="cpu"):
+    def __init__(self):
         self.model_name = "nvidia/parakeet-tdt-0.6b-v3"
         self.model = None
 
     def download(self):
-        print("Loading Parakeet model...")
-
         self.model = nemo_asr.models.ASRModel.from_pretrained(self.model_name)
-
-        self.model.change_decoding_strategy({
-            "strategy": "greedy"
-        })
 
     def transcribe(self, audio_path: str):
         if self.model is None:
             self.download()
 
-        result = self.model.transcribe([audio_path])
-        return result[0]
+        return self.model.transcribe([audio_path])[0]
         
